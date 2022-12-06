42c3c9554d84521363ac861a68f6532611162c38
Ayisha Rinsiya <69690683+AyishaRinsiya@users.noreply.github.com>
2022-12-06 12:12:12 +0530

    Update main.js

--- Addition & Area of a circle/main.js
+++ Addition & Area of a circle/main.js
@@ -1,1 +1,27 @@
+const express= require('express')
+const app=express()
+app.use(express.urlencoded())
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.get('/',(req,res)=>{
+    res.render('add',{'result':""});
+})
 
+app.post('/addpost',(req,res,next)=>{
+    var num1=req.body.fnum;
+    var num2=req.body.lnum;
+    var c=parseFloat(num1)+parseFloat(num2);
+    res.render('add',{'result':c})
+})
+app.get('/circle',(req,res,next)=>{
+    res.render('circle',{'result':""});
+})
+app.post('/area',(req,res,next)=>{
+    var r=req.body.num;
+    var c=3.14 * r * r;
+    console.log(c);
+    res.render('circle',{'result':c})
+})
+app.listen(9000,()=>{
+    console.log("Server running http://localhost:9000/")
+})
